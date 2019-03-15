3ec84bd6491a5b996ecc3bccd3ac347df17830f5
Joshua Byrd <phocks@gmail.com>
2019-03-15 10:02:24 +1000

    Let's try TypeScript

---
+++ index.ts
@@ -0,0 +1,9 @@
+console.log("Hello ٩(●ᴗ●)۶")
+
+// This import loads the firebase namespace along with all its type information.
+import * as firebase from 'firebase/app';
+ 
+// These imports load individual services into the firebase namespace.
+import 'firebase/auth';
+import 'firebase/database';
+
