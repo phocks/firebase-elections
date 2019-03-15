ceac49a8b4badcb849842eadf552e80bc12712d9
Joshua Byrd <phocks@gmail.com>
2019-03-15 11:41:30 +1000

    Adding firebase functions

---
+++ server/functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from 'firebase-functions';
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
--- server/index.js
+++ server/index.js
@@ -14,4 +14,6 @@ const app = firebase.initializeApp({
 
 const database = firebase.database();
 
-database.ref("test").set("This is coming from the server...");
+const update = { test: "testing" };
+
+database.ref("test").set(update);
